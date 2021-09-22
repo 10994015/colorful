a41de45df05f628a0804e40752f03829adfa9c06
10994015 <a0938599191@gmail.com>
2021-09-22 16:24:17 +0800

    0922 04:24

--- layout/CART/assets/js/cartPage.js
+++ layout/CART/assets/js/cartPage.js
@@ -1,8 +1,12 @@
 var cardel = document.getElementsByClassName('cardel');
-    var closeConvert = document.getElementsByClassName('closeConvert')[0];
-    var enterCode = document.getElementsByClassName('enterCode')[0];
-    var convert = document.getElementsByClassName('convert')[0];
+var closeConvert = document.getElementsByClassName('closeConvert')[0];
+var enterCode = document.getElementsByClassName('enterCode')[0];
+var convert = document.getElementsByClassName('convert')[0];
+var shippingMethodSelect = document.getElementById('shippingMethodSelect');
+var superBusiness = document.getElementsByClassName('superBusiness')[0];
+
     for(var i=0;i<cardel.length;i++){
+    
       cardel[i].addEventListener("click",delCart);
     }
 
@@ -22,3 +26,15 @@ var cardel = document.getElementsByClassName('cardel');
       convert.style.opacity=0;
       
     });
+    superBusiness.style.display="none";
+    shippingMethodSelect.addEventListener('change',(e)=>{
+      clearSuper();
+      if(e.target.value==="7-11"){
+        superBusiness.style.display="block";
+      }
+    })
+
+    function clearSuper(){
+      superBusiness.style.display="none";
+
+    }
