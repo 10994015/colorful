1b0e0b7de6139131590e3d4f6c06b6a1e60b2963
10994015 <a0938599191@gmail.com>
2021-09-30 19:07:02 +0800

    更新一下

--- layout/product_2/assets/js/promotions.js
+++ layout/product_2/assets/js/promotions.js
@@ -1,14 +1,18 @@
-var promotions = document.querySelector('.productpromotions a');
+var promotions = document.getElementsByClassName('openPromotionsBox');
 var promotionClose = document.getElementsByClassName("promotionClose")[0];
 var promotionsBoxBack = document.getElementsByClassName('promotionsBoxBack')[0];
 var promotionsBox = document.getElementsByClassName('promotionsBox')[0];
-promotions.addEventListener("click",()=>{
+
+for(var p=0;p<promotions.length;p++){
+    promotions[p].addEventListener("click",()=>{
     if(promotionsBox.style.display==="block"){
         promotionsBox.style.display="none";
     }else{
         promotionsBox.style.display="block";
     }
 })
+}
+
 promotionClose.addEventListener("click",()=>{
     promotionsBox.style.display="none";
 })
