fe865da95649859d37c393ef2c69e28426a27f9a
10994015 <a0938599191@gmail.com>
2021-10-07 00:21:06 +0800

    側欄修改完成

--- layout/LEFTSIDEBAR/assets/js/leftsidebar.js
+++ layout/LEFTSIDEBAR/assets/js/leftsidebar.js
@@ -3,6 +3,16 @@ const leftSidebarClose = document.querySelector('.left-sidebar-close');
 const leftSidebar = document.querySelector('.left-sidebar'); 
 const filter = document.getElementsByClassName('filter')[0];
 
+const prodTwo = document.getElementsByClassName('prod-class-list-two');
+const proThree = document.querySelectorAll('.prod-class-list-three');
+console.log(proThree[0]);
+for(var p=0;p<proThree.length;p++){
+    prodTwo[p].addEventListener("click",function(){
+        proThree[Number(this.className.substr(23))-1].classList.toggle('open');
+        proThree[Number(this.className.substr(23))-1].classList.toggle('close');
+    })
+}
+
 filter.addEventListener("click",()=>{
     if(leftSidebar.style.display==="block"){
         leftSidebar.style.left = "-300px";
