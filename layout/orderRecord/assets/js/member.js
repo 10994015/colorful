1a9dd91b8aedbfe8e3baf159a2de00ab76fa385d
10994015 <a0938599191@gmail.com>
2021-10-07 23:33:46 +0800

    更改bootstrap

--- layout/orderRecord/assets/js/member.js
+++ layout/orderRecord/assets/js/member.js
@@ -1,6 +1,5 @@
 const memberTitle = document.querySelector(".member-title p");
 const memberNavA = document.getElementsByClassName("memberNavA");
-console.log(memberNavA[0].innerText);
 for(var i=0;i<memberNavA.length;i++){
     memberNavA[i].innerText;
 
